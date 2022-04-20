d3cc49fb58adb329921e8db188b947f4f547c279
Alan Thornburg <alan.thornb@gmail.com>
2022-04-20 19:12:43 -0400

    Added store base class

---
+++ src/app/store/store.ts
@@ -0,0 +1,35 @@
+import { BehaviorSubject, Observable } from "rxjs"
+import { distinctUntilChanged, map } from "rxjs/operators";
+
+export interface State {
+    [key: string]: any
+}
+
+export abstract class Store {
+
+    private subject: BehaviorSubject<State>;
+    private store: Observable<State>;
+
+    constructor(initialState: State) {
+        this.subject = new BehaviorSubject<State>(initialState)
+        this.store = this.subject.asObservable().pipe(distinctUntilChanged());
+    }
+
+    select<T>(name: string): Observable<T> {
+        return this.store.pipe(
+            map((state: State) => state[name]),
+            distinctUntilChanged<T>()
+        );
+    }
+
+    set(name: string, state: any) {
+        this.subject.next({
+            ...this.value, 
+            [name]: state
+        });
+    }
+
+    get value(): State {
+        return this.subject.value;
+    }
+}
