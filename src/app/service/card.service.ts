59a8c730f3c8bf580301e13627e618e0381978ad
aathornburg <alan.thornb@gmail.com>
2022-04-21 20:09:20 -0400

    Fixed shuffling

--- src/app/service/card.service.ts
+++ src/app/service/card.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
 import { combineLatest, Observable } from 'rxjs';
-import { first, tap } from 'rxjs/operators';
+import { first, map, tap } from 'rxjs/operators';
 import { Card } from '../model/card.model';
 import { Hand } from '../model/hand.model';
 import { Player } from '../model/player.model';
 import { CardStore } from '../store/card.store';
-import { GameManagerService } from './game-manager.service';
 import { TeamService } from './team.service';
 
 @Injectable({
@@ -23,7 +22,7 @@ export class CardService {
   public shuffleDeck(): void {
     this.deck$.pipe(
       first(),
-      tap((deck: Card[]) =>
+      map((deck: Card[]) =>
         deck
           .map((card: Card) => ({ card, sort: Math.random() }))
           .sort((a, b) => a.sort - b.sort)
@@ -39,7 +38,6 @@ export class CardService {
     ]).pipe(
       first(),
       tap(([ deck, players ]: [ Card[], Player[] ]) => {
-        // not shuffled?!
         console.log("Dealing " + JSON.stringify(deck[0]) + ", " + JSON.stringify(deck[1]) + " to player " + JSON.stringify(players[0]));
       })
     ).subscribe();
